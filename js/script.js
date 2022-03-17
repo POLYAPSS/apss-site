80f26d77201e903e8d4f83851fa8d4743ff246d5
Andrew <andrew.li@egusi.com.hk>
2022-03-18 01:36:11 +0800

    added real time update feature

--- js/script.js
+++ js/script.js
@@ -118,6 +118,7 @@ function renderViwerCount(viewerCount) {
 }
 
 var viewerCount = 0;
+var gitToken;
 $(document).ready(function () {
     if (editorEnabled == "true") {
         var decodedAuth = atob(authKey);
@@ -126,6 +127,7 @@ $(document).ready(function () {
 //        console.log(decodedAuth);
 //        console.log("decodedAuth", decodedAuth);
 //        console.log("time", secondsNow - parseInt(decodedAuth.split("|")[1]));
+        gitToken = "Bearer " + decodedAuth.split("|")[0];
         if (decodedAuth.split("|")[2] != "Lemonade") {
             alert("You don't have permission to edit this page, redirecting...");
             window.location.href = baseUrl;
@@ -205,4 +207,85 @@ function hash(string) {
         console.log("IP Hash:", hashHex);
         getSHA(hashHex);
     });
+}
+
+$(document).on("click", "#beagle-update-site-button", function () {
+    getSiteSha();
+});
+
+function getSiteSha() {
+    var pathName = location.pathname;
+    console.log("Detecting Page: ", pathName);
+//    console.log("Full Path: ", "https://api.github.com/repos/POLYAPSS/apss-site/contents" + pathName);
+    var settings = {
+        "url": "https://api.github.com/repos/POLYAPSS/apss-site/contents" + pathName,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": "Basic UE9MWUFQU1M6c29jaWFsd29yazIwMjE="
+        },
+    };
+    $.ajax(settings).done(function (response) {
+        console.log("retrieved SHA: ", response.sha);
+        updateSite(response.sha);
+    }).fail(function (jqXHR, textStatus) {
+        console.log(jqXHR);
+    });
+}
+
+function updateSite(SHA) {
+//    console.log("updateSiteHTML", updateSiteHTML());
+    var fullHTML = updateSiteHTML();
+    var pathName = location.pathname;
+    var settings = {
+        "url": "https://api.github.com/repos/POLYAPSS/apss-site/contents" + pathName,
+        "method": "PUT",
+        "timeout": 0,
+        "headers": {
+            "Authorization": gitToken,
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "message": "Update Site",
+            "content": fullHTML,
+            "sha": SHA
+        }),
+     };
+
+    $.ajax(settings).done(function (response) {
+        console.log("Page Updated!", pathName);
+    }).fail(function (jqXHR, textStatus) {
+        console.log(jqXHR);
+    });
+}
+
+function updateSiteHTML(elementSelectorInput) {
+    var elementSelector = elementSelectorInput || "html";
+    var selectedBeagleEle = $(".beagle-selected");
+    $(".beagle-selected").removeClass("beagle-selected");
+    $(".beagle-hovering").removeClass("beagle-hovering");
+    var cleansedHtml = $(elementSelector).clone();
+    cleansedHtml.find(".beagle-builder").remove();
+    cleansedHtml.find(".beagle-translatable").removeClass("beagle-translatable");
+    var mainContentHtml = cleansedHtml.find("#main-html-content").html();
+    var mainContentHtmlClass = cleansedHtml.find("#main-html-content").attr("class");
+    var mainContentHtmlStyles = cleansedHtml.find("#main-html-content").attr("style");
+    cleansedHtml.find("#main-html-content").remove();
+    cleansedHtml.find("body").prepend(mainContentHtml);
+    cleansedHtml.find("body").attr("class", mainContentHtmlClass);
+    cleansedHtml.find("body").attr("style", mainContentHtmlStyles);
+    cleansedHtml.find("#main-html").remove();
+    cleansedHtml.find("*[class='']").removeAttr('class');
+    cleansedHtml.find("*[beagle-translatable='']").removeAttr('beagle-translatable');
+    var elHtml = cleansedHtml.html();
+    console.log("Preparing html file...");
+    console.log(elHtml);
+    return btoa(unescape(encodeURIComponent(elHtml)));
+//    var link = document.createElement('a');
+//    var mimeType = 'text/plain';
+//    link.setAttribute('download', 'index.html');
+//    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+//    link.click();
+//    link.remove();
+//    selectedBeagleEle.addClass("beagle-selected");
 }
