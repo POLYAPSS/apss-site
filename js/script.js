9df1aecf1daf0f1c1389f7c42cfc657cae3db0b5
Andrew <socialwork.apss@gmail.com>
2022-03-24 20:28:48 +0800

    added line divider component
    added line divider component

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var url = new URL(url_string);
 var editorEnabled = url.searchParams.get("editor");
 var byPassCounter = url.searchParams.get("stealth");
 var authKey = url.searchParams.get("auth");
-var components = ["title_and_paragraph.html", "two_lists.html", "simple_title.html", "resources_item.html", "line_divider.html", "list.html", "video_item.html", "video.html", "tip_item_left.html", "tip_item_right.html"];
+var components = ["title_and_paragraph.html", "two_lists.html", "simple_title.html", "resources_item.html", "line_divider.html", "line_divider_light.html", "list.html", "video_item.html", "video.html", "tip_item_left.html", "tip_item_right.html"];
 
 console.log(editorEnabled);
 if (editorEnabled == "true") {
